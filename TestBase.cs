5d7c47994a43e1a665052bbeec8986d24cfc58a8
bhaskarsamudrala <38501524+bhaskarsamudrala@users.noreply.github.com>
2023-02-15 15:13:21 +0530

    Add files via upload

---
+++ TestBase.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.MarkupUtils;
+using Company.Platform.automation.App.web.Utils;
+using Microsoft.Dynamics365.UIAutomation.Browser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using static Company.Platform.automation.App.web.ReportsHelper.ReportService;
+using static Company.Platform.automation.App.web.ReportsHelper.ReportLog;
+using Company.Platform.automation.App.web.Common;
+using Microsoft.Dynamics365.UIAutomation.Api.UCI;
+using Company.Platform.automation.App.web.ReportsHelper;
+
+namespace Company.Platform.automation.App.web.Tests
+{
+    public class AppStarUp
+    {
+        public TestContext TestContext { get; set; }
+
+        //protected PowerApp PwrApp { get; set; }
+
+        protected SeleniumActions SeleniumActions { get; set; }
+        public static int ThinkTime => TestSettings.Options.DefaultThinkTime;
+        public static Random Random = new Random();
+        public static AppUrl PowerAppSelected()
+        {
+            return _PowerAppSelected;
+        }
+        private static string TestCaseName { get; set; }
+        private static string TestUserId { get; set; }
+        private static List<string> SecurityRoles { get; set; }
+        private static Uri LoginUrl { get; set; }
+
+        private static AppUrl _PowerAppSelected { get; set; }
+
+        protected static ExtentReports ExtentReports;
+        protected static ExtentTest TestParent;
+        protected static ExtentTest ExtentTest;
+
+        private WebClient Client;
+
+
+        [TestInitialize]
+        public async Task SetupAsync()
+        {
+            try
+            {
+                Client = new WebClient(TestSettings.Options);
+                //PwrApp = new PowerApp(Client);
+                SeleniumActions = new SeleniumActions(Client.Browser.Driver);
+
+                TestCaseName = TestContext.TestName.Substring(4, 5);
+                TestUserId = TestSettings.TestUserId;
+                LoginUrl = TestSettings.PowerAppUrl;
+
+                //Change this assignment for the different environments, after providing a URI in the switch statement
+                _PowerAppSelected = AppUrl.Qa;
+
+                switch (_PowerAppSelected)
+                {
+                    case AppUrl.Qa:
+                        LoginUrl = new Uri("");
+                        break;
+
+                    case AppUrl.Dev:
+                        LoginUrl = new Uri(" ");
+                        break;
+
+                    case AppUrl.Uat:
+                        LoginUrl = new Uri(" ");
+                        break;
+                }
+
+                // Go ahead and run tests cases against the chosen environment.
+                //pwrApp.OnlineLogin.Login(_loginUrl);
+                PwrApp.OnlineLogin.Login(LoginUrl,  TestSettings.PowerAppAdmin.UName,  TestSettings.PowerAppAdmin.PWord);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+        }
+
+
+        [AssemblyInitialize]
+        public static void TestAssemblyInit(TestContext testContext)
+        {
+            DirUtils.CleanDirectory(DirUtils.GetProjectReportsDirectory());
+        }
+
+        [AssemblyCleanup]
+        public static void TestAssemblyClean()
+        {
+            //report flush
+            Instance.Flush();
+        }
+
+
+        [TestCleanup]
+        public virtual void TestCleanUp()
+        {
+            try
+            {
+                switch (TestContext.CurrentTestOutcome)
+                {
+                    case UnitTestOutcome.Error:
+                        ReportLog.Log(Status.Error, "Failed - Error");
+                        AddScreenShot(Client, "Failed");
+                        break;
+                    case UnitTestOutcome.Failed:
+                        ReportLog.Log(Status.Error, "Failed");
+                        AddScreenShot(Client, "Failed");
+                        break;
+                    case UnitTestOutcome.Passed:
+                        ReportLog.Log(Status.Pass, " Passed");
+                        break;
+                    case UnitTestOutcome.Timeout:
+                        ReportLog.Log(Status.Info, "Failed with Timeout issues");
+                        AddScreenShot(Client, "Failed");
+                        break;
+                    case UnitTestOutcome.Inconclusive:
+                        ReportLog.Log(Status.Info, "Failed - Inconclusive");
+                        AddScreenShot(Client, "Failed");
+                        break;
+                    default:
+                        ReportLog.Log(Status.Fail, "Failed - unknown");
+                        AddScreenShot(Client, "Failed - unknown");
+                        break;
+                }
+            }
+            catch (Exception exception)
+            {
+                throw new Exception("Exception Message: " + exception);
+            }
+        }
+
+        private void AddScreenShot(WebClient client, string title)
+        {
+            var filename = Guid.NewGuid();
+            var projectPath = DirUtils.GetProjectReportsDirectory();
+            var filePath = Path.Combine(projectPath, $"{filename}.png");
+            client.Browser.Driver.WaitForTransaction(new TimeSpan(0, 0, 5));
+            client.Browser.TakeWindowScreenShot(filePath, ScreenshotImageFormat.Png);
+            var imgBase64String = GetBase64StringForImage(filePath);
+            GetMethod().Info(title + "<BR>",
+                MediaEntityBuilder.CreateScreenCaptureFromBase64String(imgBase64String).Build());
+        }
+
+        public void LogExceptionAndFail(Exception exception)
+        {
+            var message = exception.Message + Environment.NewLine + exception.StackTrace.Trim();
+            var markup = MarkupHelper.CreateCodeBlock(message);
+            GetMethod().Log(Status.Error, markup);
+            throw exception;
+        }
+
+        public void LogException(Exception exception, WebClient client)
+        {
+            AddScreenShot(client, "Test failed from Log Exception");
+            var message = exception.Message + Environment.NewLine + exception.StackTrace.Trim();
+            var markup = MarkupHelper.CreateCodeBlock(message);
+            GetMethod().Log(Status.Error, markup);
+            throw exception;
+        }
+
+        private static string GetBase64StringForImage(string imgPath)
+        {
+            var imageBytes = File.ReadAllBytes(imgPath);
+            var based64String = Convert.ToBase64String(imageBytes);
+            return based64String;
+        }
+
+        public enum AppUrl
+        {
+            Qa,
+            Dev,
+            Uat
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            PwrApp.Browser.Driver?.Quit();
+        }
+    }
+}
---
+++ TestSettings.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Security;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.MarkupUtils;
+using Company.Platform.automation.App.web.Utils;
+using Microsoft.Dynamics365.UIAutomation.Browser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using OpenQA.Selenium;
+
+namespace Company.Platform.automation.App.web
+{
+    internal static class TestSettings
+    {
+        public static Uri PowerAppUrl { get; set; }
+        public static string AzureDevopsToken { get; set; }
+        public static string AzureTenantId { get; set; }
+        public static string KeyVaultName { get; set; }
+        public static string TestUserId { get; set; }
+        public static class SystemAdmin
+        {
+            public static SecureString UName { get; set; }
+            public static SecureString PWord { get; set; }
+        }
+        public static class NonAdmin
+        {
+            public static SecureString UName { get; set; }
+            public static SecureString PWord { get; set; }
+        }
+        public static string Type { get; set; }
+        public static string RemoteType { get; set; }
+        public static string RemoteHubServerUrl { get; set; }
+
+        public static class PowerAppAdmin
+        {
+            public static readonly SecureString UName = System.Configuration.ConfigurationManager.AppSettings["AdminUsername"].ToSecureString();
+            public static readonly SecureString PWord = System.Configuration.ConfigurationManager.AppSettings["AdminPassword"].ToSecureString();
+        }
+
+        public static readonly BrowserOptions Options = new BrowserOptions
+        {
+            PrivateMode = false,
+            FireEvents = false,
+            Headless = false,
+            UserAgent = false,
+            DefaultThinkTime = 2000,
+            BrowserType = (BrowserType)Enum.Parse(typeof(BrowserType), "Chrome"),
+            RemoteBrowserType = (BrowserType)Enum.Parse(typeof(BrowserType), "Chrome"),
+            RemoteHubServer = new Uri("http://:4444/wd/hub"),
+            UCITestMode = true,
+            StartMaximized = false,
+            //DriversPath = /*DriversPath*/Path.IsPathRooted(DriversPath) ? DriversPath : Path.Combine(Directory.GetCurrentDirectory(), DriversPath)
+            Height = 1080,
+            Width = 1920
+        };
+
+
+        public static string GetRandomString(int minLen, int maxLen)
+        {
+            var Alphabet = ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz0123456789").ToCharArray();
+            var m_randomInstance = new Random();
+            var m_randLock = new object();
+
+            var alphabetLength = Alphabet.Length;
+            int stringLength;
+            lock (m_randLock) { stringLength = m_randomInstance.Next(minLen, maxLen); }
+            var str = new char[stringLength];
+
+            // max length of the randomizer array is 5
+            var randomizerLength = (stringLength > 5) ? 5 : stringLength;
+
+            var rndInts = new int[randomizerLength];
+            var rndIncrements = new int[randomizerLength];
+
+            // Prepare a "randomizing" array
+            for (int i = 0; i < randomizerLength; i++)
+            {
+                int rnd = m_randomInstance.Next(alphabetLength);
+                rndInts[i] = rnd;
+                rndIncrements[i] = rnd;
+            }
+
+            // Generate "random" string out of the alphabet used
+            for (int i = 0; i < stringLength; i++)
+            {
+                int indexRnd = i % randomizerLength;
+                int indexAlphabet = rndInts[indexRnd] % alphabetLength;
+                str[i] = Alphabet[indexAlphabet];
+
+                // Each rndInt "cycles" characters from the array, 
+                // so we have more or less random string as a result
+                rndInts[indexRnd] += rndIncrements[indexRnd];
+            }
+            return (new string(str));
+        }
+
+        public static string GetRandomStringNumber(int length)
+        {
+            {
+                var random = new Random();
+                var rNumber = string.Empty;
+                for (var i = 0; i < length; i++)
+                    rNumber = string.Concat(rNumber, random.Next(10).ToString());
+                return rNumber;
+            }
+        }
+
+        public static class UciAppName
+        {
+            public const string CompanyApplications = "Company Applications";
+        }
+    }
+}
